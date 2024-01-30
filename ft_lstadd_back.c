b1cd57a1b39956059baa27efe56ed8ed2e949956
Tigran Yavroyan <tyavroya@c2r6s1.42yerevan.am>
2024-01-30 14:34:53 +0400

    finally done libft

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: tyavroya <tyavroya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 19:17:06 by tyavroya          #+#    #+#             */
-/*   Updated: 2024/01/30 13:09:22 by tyavroya         ###   ########.fr       */
+/*   Updated: 2024/01/30 14:32:34 by tyavroya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	if (!lst)
+	if (!lst || !new)
 		return ;
 	if (!(*lst))
+	{
 		*lst = new;
+		return ;
+	}
 	ft_lstlast(*lst)->next = new;
-	new->next = 0;
 }
